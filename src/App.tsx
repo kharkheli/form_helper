9801eeff2e4cba911960bb972366ed3dccd7d9ef
khaekheli <iliakharkheli6@gmail.com>
2022-12-10 09:09:22 +0400

    add feature like filtering and disabling select

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {Select} from './Select';
-import {Option} from './Select/types';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Select } from "./Select";
+import { Option } from "./Select/types";
 
-const options: Option[] = [{
-  label: 'Option 1',
-  value: 'option1'
-}]
+const options: Option[] = [
+  {
+    label: "Option 1",
+    value: "option1",
+  },
+];
 
 function App() {
   return (
-    <div className="App" style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '500px'}}>
-      <Select options={options} />
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "500px",
+      }}
+    >
+      <Select options={options} disabled={false} loading={false} />
     </div>
   );
 }
--- src/Select/Select.tsx
+++ src/Select/Select.tsx
@@ -1,27 +1,85 @@
-import { type } from "@testing-library/user-event/dist/type";
-import React, {useState, useMemo} from "react";
-import { Container, OptionsContainer, SingleOption } from "./style";
-import {Props, Option} from "./types";
+import React, {
+  useState,
+  useMemo,
+  useEffect,
+  useCallback,
+  useRef,
+} from "react";
+import {
+  Container,
+  OptionsContainer,
+  SingleOption,
+  Indicator,
+  LoadingContainer,
+} from "./style";
+import { Props, Option } from "./types";
+import ReactLoading from "react-loading";
+import { IoIosArrowDown } from "react-icons/io";
 
+export default function Select({ options, loading, disabled }: Props) {
+  const [focused, setFocused] = useState<boolean>(true);
+  const [inputValue, setInputValue] = useState<string>("");
+  const inputRef = useRef<HTMLInputElement>(null);
 
-export default function Select({options}: Props) {
-    const [focused, setFocused] = useState<boolean>(false);
-    const [inputValue, setInputValue] = useState<string>('');
+  const filteredOptions = useMemo(() => {
+    return options.filter((option: Option) =>
+      option.label.toLowerCase().startsWith(inputValue.toLowerCase())
+    );
+  }, [options, inputValue]);
 
-    const filteredOptions = useMemo(() => {
-        return options.filter((option: Option) => option.label.toLowerCase().startsWith(inputValue.toLowerCase()));
-    }, [options, inputValue]);
+  return (
+    <Container
+      bg="#fff"
+      className="hello"
+      outline={true}
+      onClick={() => {
+        if (disabled) return;
+        inputRef.current?.focus();
+        setFocused(true);
+      }}
+    >
+      <input
+        ref={inputRef}
+        type="text"
+        disabled={disabled}
+        onFocus={() => setFocused(true)}
+        onBlur={() => setFocused(false)}
+        placeholder="Select..."
+        value={inputValue}
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+      />
+      {loading && (
+        <LoadingContainer>
+          <ReactLoading type="bubbles" color="#ccc" height={30} width={30} />
+        </LoadingContainer>
+      )}
+      <Indicator focused={focused}>
+        <IoIosArrowDown />
+      </Indicator>
 
-    return (<Container bg='#fff' className="hello" outline={true}>
-        <input type="text" onFocus={()=>setFocused(true)} onBlur={()=>setFocused(false)} value={inputValue} onChange={(e)=>{
-            if(e.target.value === '')
-            setInputValue(e.target.value);
-        }} />
-        <OptionsContainer display={focused}>
-            {filteredOptions.map((option: Option) => {
-                return <SingleOption key={option.value}>{option.label}</SingleOption>
-            })}
-
-        </OptionsContainer>
-    </Container>)
+      <OptionsContainer display={focused}>
+        {loading ? (
+          <SingleOption
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <ReactLoading type="bubbles" color="#fff" height={30} width={30} />
+          </SingleOption>
+        ) : filteredOptions.length > 0 ? (
+          filteredOptions.map((option: Option) => {
+            return (
+              <SingleOption key={option.value}>{option.label}</SingleOption>
+            );
+          })
+        ) : (
+          <SingleOption>No options</SingleOption>
+        )}
+      </OptionsContainer>
+    </Container>
+  );
 }
