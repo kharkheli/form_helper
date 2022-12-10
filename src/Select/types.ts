06c0daa6d730e0edcec3d4e3745778f92a5435cb
khaekheli <iliakharkheli6@gmail.com>
2022-12-10 09:07:42 +0400

    add  options to controll loading and disable input

--- src/Select/types.ts
+++ src/Select/types.ts
@@ -1,13 +1,15 @@
 export type Option = {
-    label: string,
-    value: symbol | string | number,
-}
-
+  label: string;
+  value: symbol | string | number;
+};
 
 export type Props = {
-    options: Option[],
-    width?: number,
-    height?: number,
-    placeholder?: string,
-    onChange?: (value: symbol | string | number) => void,
-}
+  options: Option[];
+  width?: number;
+  height?: number;
+  placeholder?: string;
+  onChange?: (value: symbol | string | number) => void;
+  loading?: boolean;
+  separator?: boolean;
+  disabled?: boolean;
+};
